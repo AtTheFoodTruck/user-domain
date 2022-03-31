b9f093bf5c5290f372193322c09f2fca6dd7f5b0
jaemin2 <fkdlem524@naver.com>
2022-03-31 21:22:51 +0900

    라이브러리 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# user-domain
+# User-Domain
+
+FoodTruck 서비스의 User-Domain을 담당하는 Project입니다
---
+++ src/main/java/com/sesac/foodtruckuser/FoodtruckUserApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.foodtruckuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtruckUserApplication.class, args);
+    }
+
+}
